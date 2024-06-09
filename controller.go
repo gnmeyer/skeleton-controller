4ef6bdf5e3b1fed465a93a9eaa867bb6f365f902
gnmeyer <grant.meyer@liatrio.com>
2024-06-08 18:19:15 -0700

    feat: create service on deployment

--- controller.go
+++ controller.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
@@ -30,8 +34,8 @@ func newController(clientset kubernetes.Interface, depInformer appsinformers.Dep
 
 	depInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    handleAdd,
-			DeleteFunc: handleDel,
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDel,
 		},
 	)
 	return c
@@ -52,14 +56,84 @@ func (c *controller) run(ch <-chan struct{}) {
 }
 
 func (c *controller) worker() {
+	for c.processItem() {
+
+	}
+}
+
+func (c *controller) processItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	//forgot item in queue to prevent double processing
+	defer c.queue.Forget(item)
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		fmt.Printf("getting key from cache %s\n", err.Error())
+	}
+
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Printf("splitting key %s\n", err.Error())
+		return false
+	}
+
+	err = c.syncDeployment(ns, name)
+	if err != nil {
+		fmt.Printf("syncing deployment %s\n", err.Error())
+		return false
+	}
+	return true
 
 }
 
-func handleAdd(obj interface{}) {
+func (c *controller) syncDeployment(ns, name string) error {
+	ctx := context.Background()
+
+	dep, err := c.depLister.Deployments(ns).Get(name)
+	if err != nil {
+		fmt.Printf("getting deployment %s\n", err.Error())
+	}
+
+	//create service
+	// we have to modify this, to figure out the port
+	// our deployment's container is listening on
+	svc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dep.Name,
+			Namespace: ns,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: depLabels(*dep),
+			Ports: []corev1.ServicePort{
+				corev1.ServicePort{
+					Name: "http",
+					Port: 80,
+				},
+			},
+		},
+	}
+	_, err = c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("creating service %s\n", err.Error())
+	}
+
+	//create ingress
+
+	return nil
+}
+
+func depLabels(dep appsv1.Deployment) map[string]string {
+	return dep.Spec.Template.Labels
+}
+
+func (c *controller) handleAdd(obj interface{}) {
 	fmt.Println("add was called")
+	c.queue.Add(obj)
 }
 
-func handleDel(obj interface{}) {
+func (c *controller) handleDel(obj interface{}) {
 	fmt.Println("delete was called")
-
+	c.queue.Add(obj)
 }
